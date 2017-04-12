0d772c21cbc984d5b432adc401eef73d1d941a75
siriphat0homnan <siriphat_homnan@cmu.ac.th>
2017-04-12 13:53:18 +0700

    Ok debug send blood

--- projectDB/HostAdmin.php
+++ projectDB/HostAdmin.php
@@ -65,18 +65,18 @@
 <section id="intro">
   <div class="container ">
     <div class="col text-center navbar col-md-3 col-sm-3 " >
-     <div class="row text-left pad-row">
-      <div class="alert alert-warning"> 
-        Name Hospital: <?php echo $hos; ?> <p>
-        Name Admin: xxxxxxx  xxxxxxx<p>
+       <div class="row text-left pad-row">
+          <div class="alert alert-warning"> 
+            Name Hospital: <?php echo $hos; ?> <p>
+            Name Admin: xxxxxxx  xxxxxxx<p>
+        </div>
+
+        <div class="alert alert-warning"> 
+            <center><input type="submit" value="แก้ไขข้อมูลผู้บริจาค"></center>
+        </div>
+
+
     </div>
-
-    <div class="alert alert-warning"> 
-        <center><input type="submit" value="แก้ไขข้อมูลผู้บริจาค"></center>
-    </div>
-
-
-</div>
 </div>
 
 <!-- end left profile admin -->
@@ -92,58 +92,58 @@ $result = $conn->query($sql);
 
 
     <form method="post" action="sendblood.php">
-       <div class="row text-left pad-row">
-           
-           <div class="alert alert-warning"> 
-              โรงพยาบาล:
-              <select id="hospital" name="hospital">
+     <div class="row text-left pad-row">
+         
+         <div class="alert alert-warning"> 
+          โรงพยาบาล:
+          <select id="hospital" name="hospital">
 
-                <?php
-                $i = 1;
-                while ($row=$result->fetch_assoc()) {
-                  $name = $row['h_name'];
-                  echo '<option value=" '.$row['username'].' ">';
-                  echo $name."</option>";
-                  $i++;
-              }
+            <?php
+            $i = 1;
+            while ($row=$result->fetch_assoc()) {
+              $name = $row['h_name'];
+              echo '<option value="'.$row['username'].'">';
+              echo $name."</option>";
+              $i++;
+          }
 
-              ?>
+          ?>
 
-          </select>
-
-      </div>
-
-
-      <div class="alert alert-warning">
-          กรุ๊ปเลือด:  
-
-          <select id="hospital" name="blood_type">
-              <option  value="O_vol">O</option>
-              <option value="A_vol">A</option>
-              <option value="B_vol">B</option>
-              <option value="AB_vol">AB</option>
-              <option value="O_neg_vol">O-</option>
-              <option value="A_neg_vol">A-</option>
-              <option value="B_neg_vol">B-</option>
-              <option value="AB_neg_vol">AB-</option>
-
-          </select>
-
-      </div>
-
-      <div class="alert alert-warning">
-          กรอกจำนวนเลือด:  <input type="text" id="num_blood" name="num_blood">
-
-      </div>
-
-      <div class="alert alert-warning">
-          ยืนยัน:  <input type="submit" value="Submit" name="submit">
-
-      </div>
-
-
+      </select>
 
   </div>
+
+
+  <div class="alert alert-warning">
+      กรุ๊ปเลือด:  
+
+      <select id="hospital" name="blood_type">
+          <option  value="O_vol">O</option>
+          <option value="A_vol">A</option>
+          <option value="B_vol">B</option>
+          <option value="AB_vol">AB</option>
+          <option value="O_neg_vol">O-</option>
+          <option value="A_neg_vol">A-</option>
+          <option value="B_neg_vol">B-</option>
+          <option value="AB_neg_vol">AB-</option>
+
+      </select>
+
+  </div>
+
+  <div class="alert alert-warning">
+      กรอกจำนวนเลือด(ml):  <input type="text" id="num_blood" name="num_blood">
+
+  </div>
+
+  <div class="alert alert-warning">
+      ยืนยัน:  <input type="submit" value="Submit" name="submit">
+
+  </div>
+
+
+
+</div>
 </div>
 </from>
 
@@ -175,55 +175,55 @@ $row = $result2->fetch_assoc();
 
 
         <div class="col text-center navbar-right col-md-2 col-sm-2 " >
-         <p>
-          <div class="col-md-12">
-            <div class="row text-center pad-row">
-              <div class="alert alert-success">
-                 <?php
-                 echo '<div class="skill-name">Group O- '.ceil($on/$div * 100)."%</div>"; 
-                 ?> 
-                 <div class="progress progress-striped active progress-adjust">
-                  <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($on/$div * 100); ?>%">
-                   <span class="sr-only">100% Complete</span>
-                   <?php echo ($on); ?>
+           <p>
+              <div class="col-md-12">
+                <div class="row text-center pad-row">
+                  <div class="alert alert-success">
+                   <?php
+                   echo '<div class="skill-name">Group O- '.ceil($on/$div * 100)."%</div>"; 
+                   ?> 
+                   <div class="progress progress-striped active progress-adjust">
+                      <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($on/$div * 100); ?>%">
+                         <span class="sr-only">100% Complete</span>
+                         <?php echo ($on); ?>
 
-               </div>
+                     </div>
 
-           </div>
+                 </div>
 
-       </div>
+             </div>
 
-       <div class="alert alert-success">
-        <?php             echo '<div class="skill-name">Group A- '.ceil($an/$div * 100).'%</div>' ;  ?> 
-        <div class="progress progress-striped active progress-adjust">
-            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($an/$div * 100); ?>%">
-              <span class="sr-only">100% Complete</span>
-              <?php echo ($an); ?>
+             <div class="alert alert-success">
+                <?php             echo '<div class="skill-name">Group A- '.ceil($an/$div * 100).'%</div>' ;  ?> 
+                <div class="progress progress-striped active progress-adjust">
+                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($an/$div * 100); ?>%">
+                      <span class="sr-only">100% Complete</span>
+                      <?php echo ($an); ?>
+
+                  </div>
+              </div>
 
           </div>
+
+          <div class="alert alert-success">
+            <?php            echo  '<div class="skill-name">Group B- '.ceil($bn/$div * 100).'%</div>'; ?> 
+            <div class="progress progress-striped active progress-adjust">
+                <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0"            aria-valuemax="100" style="width: <?php echo ceil($bn/$div * 100); ?>%">
+                  <span class="sr-only">100% Complete</span>
+                  <?php echo ($bn); ?>
+              </div>
+          </div>
+
       </div>
 
-  </div>
-
-  <div class="alert alert-success">
-    <?php            echo  '<div class="skill-name">Group B- '.ceil($bn/$div * 100).'%</div>'; ?> 
-    <div class="progress progress-striped active progress-adjust">
-        <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0"            aria-valuemax="100" style="width: <?php echo ceil($bn/$div * 100); ?>%">
-          <span class="sr-only">100% Complete</span>
-          <?php echo ($bn); ?>
-      </div>
-  </div>
-
-</div>
-
-<div class="alert alert-success">
-    <?php          echo   '<div class="skill-name">Group AB- '.ceil($abn/$div * 100).'%</div>' ; ?> 
-    <div class="progress progress-striped active progress-adjust">
-      <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0"               aria-valuemax="100" style="width: <?php echo ceil($abn/$div * 100); ?>%">
-        <span class="sr-only">100% Complete</span>
-        <?php echo ($abn); ?>
+      <div class="alert alert-success">
+        <?php          echo   '<div class="skill-name">Group AB- '.ceil($abn/$div * 100).'%</div>' ; ?> 
+        <div class="progress progress-striped active progress-adjust">
+          <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100"    aria-valuemin="0"               aria-valuemax="100" style="width: <?php echo ceil($abn/$div * 100); ?>%">
+            <span class="sr-only">100% Complete</span>
+            <?php echo ($abn); ?>
+        </div>
     </div>
-</div>
 
 </div>
 </div>
@@ -234,44 +234,44 @@ $row = $result2->fetch_assoc();
 
 <div class="col text-center navbar-right col-md-2 col-sm-2" >
   <p>
-   <div class="col-md-16">
-     <div class="col text-center pad-row">
-       <div class="alert alert-success">
-        <?php            echo '<div class="skill-name">Group O '.ceil($o/$div * 100).'%</div>'   ?> 
-        <div class="progress progress-striped active progress-adjust">
-          <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($o/$div * 100); ?>%">
-            <span class="sr-only">100% Complete</span>
-            <?php echo ($o); ?>
+     <div class="col-md-16">
+       <div class="col text-center pad-row">
+         <div class="alert alert-success">
+            <?php            echo '<div class="skill-name">Group O '.ceil($o/$div * 100).'%</div>'   ?> 
+            <div class="progress progress-striped active progress-adjust">
+              <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($o/$div * 100); ?>%">
+                <span class="sr-only">100% Complete</span>
+                <?php echo ($o); ?>
+            </div>
         </div>
+
     </div>
 
-</div>
-
-<div class="alert alert-success">
-    <?php         echo  '<div class="skill-name">Group A '.ceil($a/$div * 100).'%</div>'; ?> 
-    <div class="progress progress-striped active progress-adjust">
-        <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($a/$div * 100); ?>%">
-          <span class="sr-only">100% Complete</span>
-          <?php echo ($a); ?>
+    <div class="alert alert-success">
+        <?php         echo  '<div class="skill-name">Group A '.ceil($a/$div * 100).'%</div>'; ?> 
+        <div class="progress progress-striped active progress-adjust">
+            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($a/$div * 100); ?>%">
+              <span class="sr-only">100% Complete</span>
+              <?php echo ($a); ?>
+          </div>
       </div>
+
   </div>
 
-</div>
-
-<div class="alert alert-success">
- <div class="skill-name">Group B <?php echo ceil($b/$div * 100); ?>%</div> 
- <div class="progress progress-striped active progress-adjust">
-  <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($b/$div * 100); ?>%">
-    <span class="sr-only">100% Complete</span>
-    <?php echo ($b); ?>
-</div>
-</div>
-
-</div>
-
-<div class="alert alert-success">
-   <div class="skill-name">Group AB <?php echo ceil($ab/$div * 100); ?>%</div> 
+  <div class="alert alert-success">
+   <div class="skill-name">Group B <?php echo ceil($b/$div * 100); ?>%</div> 
    <div class="progress progress-striped active progress-adjust">
+      <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($b/$div * 100); ?>%">
+        <span class="sr-only">100% Complete</span>
+        <?php echo ($b); ?>
+    </div>
+</div>
+
+</div>
+
+<div class="alert alert-success">
+ <div class="skill-name">Group AB <?php echo ceil($ab/$div * 100); ?>%</div> 
+ <div class="progress progress-striped active progress-adjust">
     <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ceil($ab/$div * 100); ?>%">
       <span class="sr-only">100% Complete</span>
       <?php echo ($ab); ?>
@@ -287,18 +287,6 @@ $row = $result2->fetch_assoc();
 </section>
 
 <!--/.JUST-INTRO END-->
-<section  class="note-sec" >
- <div class="container">
-   <div class="row text-center pad-row" >
-    <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 ">
-      <i class="fa fa-quote-left fa-3x"></i>
-      <p>
-        อยากให้ทุกคนช่วยกันบริจาคเลือดดดดด
-    </p>
-</div>
-</div>
-</div>   
-</section>
 
 <!--/.CLIENTS END-->
 <?php include 'footer.php'; ?>
--- projectDB/sendblood.php
+++ projectDB/sendblood.php
@@ -1,21 +1,105 @@
 <?php
+session_start();
+$user = $_SESSION['U'];
 
+$hos = $_POST['hospital'];
+$blood = $_POST['blood_type'];
+$num = $_POST['num_blood'];
+$d = date("Y-m-d");
 
-    $hos = $_POST['hospital'];
-    $blood = $_POST['blood_type'];
-    $num = $_POST['num_blood'];
-
-    echo "<script type='text/javascript'>alert('$hos-$blood-$num');</script>";
+echo "<script type='text/javascript'>alert('$hos-$blood-$num');</script>";
+echo $hos."<br>".$blood."<br>".$num;
+echo "<br>".$user."<br>".date("Y-m-d")."<br>";
     // echo "<script language=\"JavaScript\">"
     // echo "alert($hos."<br>".$blood."<br>".$num)";
     // echo "</script>";
 
-    
-   include 'connect.php';
+//echo strlen($hos)."<br>";
 
-    
-        $SQL = "UPDATE hospital, user SET hospital.O_vol = 1000000 WHERE (user.username = '$hos') and (user.h_name = hospital.h_name)";
-        $result = mysql_query($SQL);
-    
- 
+
+include 'connect.php';
+
+
+//$SQL = 'update hospital, user set '.$blood.'='.$blood.'+'.$num. ' where username = '.$hos.' and user.h_name = hospital.h_name';
+
+$SQL = "update hospital, user set $blood = $blood+$num where username = '$hos' and user.h_name = hospital.h_name";
+
+
+echo $SQL."<br>";
+/*if ($conn->query($SQL)=== TRUE){
+    echo "update complete\n";
+}else{
+    echo "Update fail";
+}*/
+
+$SQL1 = "update hospital, user set $blood = $blood-$num where username = '$user' and user.h_name = hospital.h_name";
+
+echo $SQL1."<br>";
+
+if ($conn->query($SQL1)=== TRUE){
+    echo "update 1 complete";
+}else{
+    echo "Update 1s fail";
+}
+
+if ( strcmp($blood,"A_vol")==0 ){
+    $b = "A";
+}
+
+if ( strcmp($blood,"B_vol")==0 ){
+    $b = "B";
+}
+
+if ( strcmp($blood,"O_vol")==0 ){
+    $b = "O";
+}
+
+if ( strcmp($blood,"AB_vol")==0 ){
+    $b = "AB";
+}
+
+if ( strcmp($blood,"A_neg_vol")==0 ){
+    $b = "A-";
+}
+
+if ( strcmp($blood,"B_neg_vol")==0 ){
+    $b = "B-";
+}
+
+if ( strcmp($blood,"O_neg_vol")==0 ){
+    $b = "O-";
+}
+
+if ( strcmp($blood,"AB_neg_vol")==0 ){
+    $b = "AB-";
+}
+
+
+$u = "select h_name from user where username='$user'";
+
+$result = $conn->query($u);
+$row = $result->fetch_assoc();
+$ans_u= $row['h_name']; 
+
+echo $ans_u;
+
+$u = "select h_name from user where username='$hos'";
+
+$result = $conn->query($u);
+$row = $result->fetch_assoc();
+$ans_h= $row['h_name']; 
+
+echo $ans_h;
+
+
+
+
+$sql = "insert into transfer values ('$ans_u','$ans_h','$d','$b',$num)";
+
+echo $sql."<br>";
+if ($conn->query($sql)=== TRUE){
+    echo "update 2 complete\n";
+}else{
+    echo "Update 2 fail";
+}
 ?>
